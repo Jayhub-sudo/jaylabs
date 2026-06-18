e9d559e86b20e0fe272115b6ce50f7218c9865d7
Jayhub-sudo <latorrejay@yahoo.com>
2026-06-18 04:37:04 -0700

    Create first dashboard screen

--- app/App.jsx
+++ app/App.jsx
@@ -1,1 +1,8 @@
-
+export default function App() {
+    return (
+        <div>
+            <h1>JAYLABS Material Intelligence</h1>
+            <p>Asset Library Dashboard</p>
+        </div>
+    );
+}
